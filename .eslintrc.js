bf84db454202f35f63a6b133946cfec76b5cd0f9
AntoninaTrapsh <antrapsh@gmail.com>
2022-04-20 01:09:30 +0400

    Template Output

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-  'parser': '@babel/eslint-parser',
-  'parserOptions': {
-    babelOptions: {
-      configFile: './babel.config.json',
+    'parser': '@babel/eslint-parser',
+    'parserOptions': {
+        babelOptions: {
+            configFile: './babel.config.json',
+        },
     },
-  },
-  'env': {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  'extends': ['eslint:recommended', 'google'],
-  'rules': {
-    'max-len': ['error', {'code': 80}],
-    'comma-dangle': 0,
-    'eol-last': ['error', 'never'],
-    'require-jsdoc': 0,
-    'jsdoc/require-jsdoc': 0,
-  },
+    'env': {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    'extends': ['eslint:recommended', 'google'],
+    'rules': {
+        'max-len': ['error', {'code': 80}],
+        'comma-dangle': 0,
+        'eol-last': ['error', 'never'],
+        'require-jsdoc': 0,
+        'jsdoc/require-jsdoc': 0,
+        'indent': ['error', 4],
+    },
 };
