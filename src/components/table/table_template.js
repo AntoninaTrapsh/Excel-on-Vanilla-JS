ecaaae43943141ed078aa0a802de2a4533e1cd29
antonina <antrapsh@gmail.com>
2022-12-22 17:06:05 +0300

    added possibility to create table with row & column

--- src/components/table/table_template.js
+++ src/components/table/table_template.js
@@ -3,39 +3,49 @@ const CODES = {
     Z: 90,
 };
 
-function createCell() {
+function toCell() {
     return `
-        <div class="cell" contenteditable="">B2</div>
+        <div class="cell" contenteditable=""></div>
     `;
 }
 
-function createCol() {
+function toColumn(col) {
     return `
-        <div class="column">
-            A
-        </div>
+        <div class="column">${col}</div>
     `;
 }
 
-function createRow(content) {
+function createRow(content, index = '') {
     return `
         <div class="row">
-            <div class="row-info"></div>
+            <div class="row-info">${index}</div>
             <div class="row-data">${content}</div>
         </div>
 `;
 }
 
+function toChar(_, index) {
+    return String.fromCharCode(CODES.A + index);
+}
+
 export function createTable(rowsCount = 15) {
-    const colsCount = CODES.Z - CODES.A;
+    const colsCount = CODES.Z - CODES.A + 1;
     const rows = [];
 
-    const cols = new Array(colsCount).fill('');
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toColumn)
+        .join('');
 
-    rows.push(createRow());
+    rows.push(createRow(cols));
 
     for (let i = 0; i < rowsCount; i++) {
-        rows.push(createRow());
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('');
+        rows.push(createRow(cells, i + 1));
     }
     return rows.join('');
 }
